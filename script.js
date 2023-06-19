07e98a082b7d49699c5c90a0c2aae0fe279e4a4d
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-19 19:04:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+  const roper= document.getElementById("rope-lengths")
+	let lenarr= parseInt((roper.value).split(','))
+	let sum=0;
+	while(lenarr.length!=1){
+		lenarr.sort()
+		sum=lenarr[0]+lenarr[1]
+		lenarr[1]=sum
+		arr.shift()
+	}
+	sum=lenarr[0]+sum
+	cont reser= document.getElementById("result")
+	reser.innerText= sum
 }  
